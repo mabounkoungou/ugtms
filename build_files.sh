76a4332661782d4820750021fc5f6b64c1d57f93
raad <raadbouks@gmail.com>
2024-09-15 11:59:40 +0000

    update

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,8 @@
- echo "BUILD START"
- python3.12.3 -m pip install -r requirements.txt
- python3.12.3 manage.py collectstatic --noinput --clear
- echo "BUILD END"
+#!/bin/bash
+echo "BUILD START"
+
+# Use python3 or python3.12 if Python 3.12 is installed
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput --clear
+
+echo "BUILD END"
